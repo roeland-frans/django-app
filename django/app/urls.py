669464eb53f6b8ea5d744392bf76c9ecfc6c3564
Roeland van Nieuwkerk <roeland.frans@gmail.com>
2023-11-10 13:43:08 -0500

    Add tailwind support and error pages

--- django/app/urls.py
+++ django/app/urls.py
@@ -1,6 +1,9 @@
+import app.views.errors
+
 from app.views.api import ApiView
 from app.views.api import GraphiQLView
 from django.conf import settings
+from django.conf import urls
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles import views
@@ -26,4 +29,13 @@ urlpatterns += [
         GraphiQLView.as_view(graphiql=True),
         name="graphiql",
     ),
+    path("400/", lambda request: app.views.errors.error400(request, None)),
+    path("403/", lambda request: app.views.errors.error403(request, None)),
+    path("404/", lambda request: app.views.errors.error404(request, None)),
+    path("500/", app.views.errors.error500),
 ]
+
+urls.handler400 = "app.views.errors.error400"
+urls.handler403 = "app.views.errors.error403"
+urls.handler404 = "app.views.errors.error404"
+urls.handler500 = "app.views.errors.error500"
---
+++ django/tailwind/tailwind.config.js
@@ -0,0 +1,43 @@
+const colors = require("tailwindcss/colors")
+
+module.exports = {
+  content: ["../app/**/*.{html,py}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      red: colors.red,
+    },
+    extend: {
+      colors: {
+        gray: {
+          50: "#F8FAFC",
+          100: "#F1F5F9",
+          200: "#E2E8F0",
+          300: "#CBD5E1",
+          400: "#94A3B8",
+          500: "#64748B",
+          600: "#475569",
+          700: "#334155",
+          800: "#1E293B",
+          900: "#0F172A",
+        },
+        jade: {
+          50: "#ECFDF5",
+          100: "#D1FAE5",
+          200: "#A7F3D0",
+          300: "#6EE7B7",
+          400: "#34D399",
+          500: "#10B981",
+          600: "#059669",
+          700: "#047857",
+          800: "#065F46",
+          900: "#064E3B",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+}
